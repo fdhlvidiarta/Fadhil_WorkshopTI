08be14f4b63e86422549942739499d4862dd38a0
fadhil <fadhilv05@gmail.com>
2023-11-22 17:50:23 +0700

    first commit

---
+++ Fadhil_Workshop23/resources/views/dashboard.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        Nama : Fadhil Vidiarta <br>
+        Nim : 233307047 <br>
+        Kelas / Prodi : 1B / Teknologi Informasi
+    </p>
+</body>
+</html>
